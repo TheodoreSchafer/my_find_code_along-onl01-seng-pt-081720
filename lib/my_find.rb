a9fb1128669976165b2ba6cfb8a24f4c3a16abc4
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 18:15:33 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  block_return_value = []
+  while i < collection.length
+    block_return_value << yield(collection[i])
+    i += 1
+  end
 
 end
